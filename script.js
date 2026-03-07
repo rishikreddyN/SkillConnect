7e571fae61c9ce4419db778b376e8bb60f3da31e
Rishik Reddy <nallarishik58@gmail.com>
2026-03-07 22:47:27 +0530

    data changed to cloud

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ app.set("views", path.join(__dirname, "views"));
 // ------------------ Database ------------------
 
 async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/SkillConnect");
+    await mongoose.connect(process.env.MONGO_URI);
 }
 
 main()
