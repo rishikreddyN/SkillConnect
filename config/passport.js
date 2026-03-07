c7fff0d49cb8b3579ba595bbe39ca4f1ee1810e4
Rishik Reddy <nallarishik58@gmail.com>
2026-03-08 01:20:27 +0530

    commiting changes

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,10 @@ module.exports = function(){
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:8080/auth/google/callback"
+        callbackURL: 
+process.env.NODE_ENV === "production"
+? "https://skillconnect-5-aerx.onrender.com/auth/google/callback"
+: "http://localhost:8080/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done)=>{
 
